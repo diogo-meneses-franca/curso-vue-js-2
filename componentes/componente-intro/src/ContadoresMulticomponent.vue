ca9eb940db486d7e569ec071df67c0a94c57f733
Diogo Franca <diogo.meneses.franca@gmail.com>
2024-12-18 18:03:16 -0300

    feat: componentes

---
+++ componentes/componente-intro/src/ContadoresMulticomponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="contadores">
+        <app-contador v-for="index in 5" :key="index"/>
+    </div>
+</template>
+
+<script>
+    import ContadorComponent from './ContadorComponent.vue'
+    export default {
+        components: { 'app-contador': ContadorComponent }
+    }
+</script>
+
+<style>
+
+</style>
