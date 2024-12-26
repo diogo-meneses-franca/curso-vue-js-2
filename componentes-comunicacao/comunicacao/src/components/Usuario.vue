c2c484a4c1a32bc38e2983043f328dbb4b4eee67
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-12-26 08:57:13 -0300

    feat: componentes comunicacao

---
+++ componentes-comunicacao/comunicacao/src/components/Usuario.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="container">
+        <h1>Componente Usuário</h1>
+        <p>Esse é um componente muito legal!</p>
+        <p>Nome é <strong>{{ nome }}</strong></p>
+        <button @click="alterarNome">Alterar Nome</button>
+        <hr>
+        <div class="componentes">
+            <app-usuario-info :nome="nome" @nomeMudou="nome = $event.novo" :reiniciarFn="reiniciarNome" />
+            <app-usuario-editar />
+        </div>
+    </div>
+</template>
+
+<script>
+import AppUsuarioInfo from './UsuarioInfo'
+import AppUsuarioEditar from './UsuarioEditar'
+
+export default {
+    components: { AppUsuarioInfo, AppUsuarioEditar },
+    data () {
+        return {
+            nome: 'Pedro'
+        }
+    },
+    methods: {
+        alterarNome() {
+            this.nome = 'Ana'
+        },
+        reiniciarNome() {
+            this.nome = 'Pedro'
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .container {
+        background-color: #27363b;
+        color: #fff;
+        padding: 10px;
+    }
+
+    .container hr {
+        margin: 20px 10px;
+    }
+
+    .componentes {
+        display: flex;
+    }
+    /*
+    Em CSS, a seletor > * significa:
+
+        >: Um combinador filho direto. Ele seleciona apenas os elementos que são filhos diretos do elemento pai especificado.
+        *: Um seletor universal que corresponde a todos os elementos.
+
+        Assim, o seletor .componentes > * significa:
+
+        "Selecione todos os elementos que são filhos diretos do elemento com a classe componentes."
+    */
+    .componentes > * {
+        margin: 10px;
+    }
+</style>
---
+++ componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do Usuário: <strong>{{ inverterNome() }}</strong></p>
+        <button @click="reiniciarNome">Reiniciar Nome</button>
+        <button @click="reiniciarFn">Reiniciar Nome (callback)</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        nome: {
+            type: String,
+            required: true,
+            default: 'Anônimo'
+        },
+        reiniciarFn: Function
+    },
+    methods: {
+        inverterNome() {
+            return this.nome.split('').reverse().join('')
+        },
+        reiniciarNome() {
+            const antigo = this.nome
+            this.nome = 'Pedro'
+            this.$emit(
+                'nomeMudou', 
+                {
+                antigo,
+                novo: this.nome
+                }
+            )
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
