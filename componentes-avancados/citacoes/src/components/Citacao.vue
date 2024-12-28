522e3047422c5074c1c6b0bc0dffb61195597405
Diogo Franca <diogo.meneses.franca@gmail.com>
2024-12-28 16:45:37 -0300

    feat: componentes avancados

---
+++ componentes-avancados/citacoes/src/components/Citacao.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="citacao">
+        <p>{{ fonte }}</p>
+        <p>{{ texto }}</p>
+        <p>{{ autor }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        fonte: String,
+        autor: String,
+        texto: String
+    }
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+</style>
--- componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
+++ componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -3,12 +3,14 @@
         <h2>As Informações de Usuário</h2>
         <p>Vários detalhes...</p>
         <p>Nome do Usuário: <strong>{{ inverterNome() }}</strong></p>
+        <p>Idade do Usuário: <strong>{{ idade }}</strong></p>
         <button @click="reiniciarNome">Reiniciar Nome</button>
         <button @click="reiniciarFn">Reiniciar Nome (callback)</button>
     </div>
 </template>
 
 <script>
+import barramento from './barramento';
 export default {
     props: {
         nome: {
@@ -16,7 +18,8 @@ export default {
             required: true,
             default: 'Anônimo'
         },
-        reiniciarFn: Function
+        reiniciarFn: Function,
+        idade: Number
     },
     methods: {
         inverterNome() {
@@ -33,6 +36,11 @@ export default {
                 }
             )
         }
+    },
+    created() {
+        barramento.quandoIdadeMudar(idade => {
+            this.idade = idade
+        })
     }
 }
 </script>
