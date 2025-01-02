999d1e2bcb485a4a05d3bc40211709fb05c94a20
Diogo Franca <diogo.meneses.franca@gmail.com>
2025-01-02 16:35:30 -0300

    feat: inicio do projeto tarefas

---
+++ projeto-tarefas/tarefas/src/components/TaskGrid.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="tasks-grid">
+        <template v-if="tasks.length">
+            <Task v-for="task in tasks" :task="task" :key="task.name" ></Task>
+        </template>
+        <p v-else class="no-tasks">Sua vida está em dia :)</p>
+    </div>
+
+</template>
+
+<script>
+import Task from './Task.vue';
+    export default {
+        components: {Task},
+        props: {
+            tasks: {type: Array, required: true}
+        }
+    }
+</script>
+
+<style>
+.tasks-grid {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+}
+
+.tasks-grid .task {
+    margin: 10px;
+}
+
+.no-task {
+    color: #AAA;
+    font-size: 1.7rem;
+    
+}
+</style>
